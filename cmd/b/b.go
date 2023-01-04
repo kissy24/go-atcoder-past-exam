779ac6633f4c0b13c942e937edd9be77c8e8790a
kissy24 <kissy105808j@gmail.com>
2023-01-05 00:20:23 +0900

    Problem B solved.

--- cmd/b/b.go
+++ cmd/b/b.go
@@ -1,3 +1,44 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+)
+
+func inputSales() []int {
+	var period int
+	// fmt can be pre-typed
+	fmt.Scan(&period)
+	sales := make([]int, period)
+	for i := 0; i < period; i++ {
+		fmt.Scan(&sales[i])
+	}
+	return sales
+}
+
+func generateDiffs(sales []int) []int {
+	var diffs []int
+	for i := 1; i < len(sales); i++ {
+		diff := sales[i] - sales[i-1]
+		diffs = append(diffs, diff)
+	}
+	return diffs
+}
+
+func format(salesDiff int) string {
+	if salesDiff > 0 {
+		return fmt.Sprintf("up %d", salesDiff)
+	} else if salesDiff < 0 {
+		// Sign reversal is easier to read than math.Abs.
+		return fmt.Sprintf("down %d", -salesDiff)
+	} else {
+		return "stay"
+	}
+}
+
+func main() {
+	sales := inputSales()
+	salesDiffs := generateDiffs(sales)
+	for _, sd := range salesDiffs {
+		fmt.Println(format(sd))
+	}
+}
