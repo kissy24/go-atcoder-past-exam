ee35a220747b5e01b62d23a409fa87ce35ba2678
kissy24 <kissy105808j@gmail.com>
2023-01-09 14:46:05 +0900

    C Production code  has been implemented

--- cmd/c/c.go
+++ cmd/c/c.go
@@ -5,15 +5,33 @@ import (
 	"fmt"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 )
 
 var sc = bufio.NewScanner(os.Stdin)
 
+type sequence []int
+
+func ToSeq(strSlice []string) (seq sequence, err error) {
+	for _, s := range strSlice {
+		var v int
+		v, err = strconv.Atoi(s)
+		if err != nil {
+			return
+		}
+		seq = append(seq, v)
+	}
+	return
+}
+
 func main() {
 	sc.Scan()
 	var inputs []string = strings.Split(sc.Text(), " ")
-	sort.Strings(inputs)
-	strings.Join(inputs, " ")
-	fmt.Println(inputs)
+	seq, err := ToSeq(inputs)
+	if err != nil {
+		fmt.Println(err)
+	}
+	sort.Ints(seq)
+	fmt.Println(seq[3])
 }
