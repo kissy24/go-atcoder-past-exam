523caff085df4cbbdff0dbe9b6cb0acc2111cf23
kissy24 <kissy105808j@gmail.com>
2023-01-11 21:28:02 +0900

    Able to take input for D problems

--- cmd/d/d.go
+++ cmd/d/d.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+type sequence []int
+
+func inputSeq() sequence {
+	var period int
+	fmt.Scan(&period)
+	seq := make([]int, period)
+	for i := 0; i < period; i++ {
+		fmt.Scan(&seq[i])
+	}
+	return seq
+}
+
+func main() {
+	seq := inputSeq()
 }
