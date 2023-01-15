49ea0c67bed6bb3722fa7315da8b4eccf9ebd798
kissy24 <kissy105808j@gmail.com>
2023-01-15 16:20:40 +0900

    D problems solved

--- cmd/d/d.go
+++ cmd/d/d.go
@@ -3,6 +3,41 @@ package main
 import "fmt"
 
 type sequence []int
+type counter map[int]int
+
+const DUP = 2
+const MISS = 0
+
+func CountNums(seq sequence) counter {
+	c := make(map[int]int)
+	// To define missing values
+	for i := 1; i < len(seq)+1; i++ {
+		c[i] = 0
+	}
+	for _, num := range seq {
+		c[num]++
+	}
+	return c
+}
+
+func getMissNum(c counter) (k int, ok bool) {
+	for k, v := range c {
+		if v == MISS {
+			return k, true
+		}
+	}
+	return
+
+}
+
+func getDupNum(c counter) (k int, ok bool) {
+	for k, v := range c {
+		if v == DUP {
+			return k, true
+		}
+	}
+	return
+}
 
 func inputSeq() sequence {
 	var period int
@@ -16,4 +51,12 @@ func inputSeq() sequence {
 
 func main() {
 	seq := inputSeq()
+	c := CountNums(seq)
+	dupNum, _ := getDupNum(c)
+	missNum, ok := getMissNum(c)
+	if !ok {
+		fmt.Println("Collect")
+	} else {
+		fmt.Printf("%d %d\n", dupNum, missNum)
+	}
 }
--- cmd/d/d_test.go
+++ cmd/d/d_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCountNums(t *testing.T) {
+	inputs := sequence([]int{1, 2, 3, 4, 5, 5})
+	expected := map[int]int{1: 1, 2: 1, 3: 1, 4: 1, 5: 2, 6: 0}
+	actual := CountNums(inputs)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected: %v, Actual: %v", expected, actual)
+	}
+}
