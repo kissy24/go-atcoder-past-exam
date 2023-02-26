6c136784d8c3e5567297f8f7049de6683b3d15f2
kissy24 <kissy105808j@gmail.com>
2023-02-26 20:25:19 +0900

    Test described

--- cmd/e/e_test.go
+++ cmd/e/e_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestToInts(t *testing.T) {
+	inputs := []string{"1", "2", "3"}
+	expected := []int{1, 2, 3}
+	actual := ToInts(inputs)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected: %v, Actual: %v", expected, actual)
+	}
+}
