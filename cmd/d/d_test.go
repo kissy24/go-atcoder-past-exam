e948ac57694451a616a49981a82e3edd76a4aeb9
kissy24 <kissy105808j@gmail.com>
2023-03-21 20:54:31 +0900

    fix d test

--- cmd/d/d_test.go
+++ cmd/d/d_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestCountNums(t *testing.T) {
 	inputs := sequence([]int{1, 2, 3, 4, 5, 5})
-	expected := map[int]int{1: 1, 2: 1, 3: 1, 4: 1, 5: 2, 6: 0}
+	expected := counter(map[int]int{1: 1, 2: 1, 3: 1, 4: 1, 5: 2, 6: 0})
 	actual := CountNums(inputs)
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Expected: %v, Actual: %v", expected, actual)
